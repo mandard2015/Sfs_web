623efb232b8eecd416fe93a5f1dbd848f54333bf
mandard2015 <mandard1512@gmail.com>
2025-07-07 13:16:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="text-sm font-bold underline text-blue-600">
+          Hello Tailwind CSS!
+          I am Web dev
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
